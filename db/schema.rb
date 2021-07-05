aef6f215d9c051a3ee59d9d15d967aed10d02c25
Hermes Vierma <inghermesvierma@hotmail.com>
2021-07-05 14:23:59 -0500

    with filter by category working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_30_010102) do
+ActiveRecord::Schema.define(version: 2021_07_04_162928) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "cetegories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "jobs", force: :cascade do |t|
     t.string "title"
@@ -19,6 +31,7 @@ ActiveRecord::Schema.define(version: 2021_06_30_010102) do
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
   end
 
 end
